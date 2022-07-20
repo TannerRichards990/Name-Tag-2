93f8d99abc635d838c073a67d4f464e84c20a6fe
Tanner Richards <Tanner.richards990@gmail.com>
2022-07-20 15:15:22 -0700

    added button functionality to name tag, still having issues with it working properly

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // import functions and grab DOM elements
 
 const nameInput = document.getElementById('name-input');
-console.log
+console.log(nameInput);
+
+const nameDisplay = document.getElementById('name-display');
+console.log(nameDisplay);
+
+const updateButton = document.getElementById('update-btn');
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+nameInput.addEventListener('input' , () => {
+    const name = nameInput.value;
+    console.log(name);
+    nameDisplay.textContent = name;
+});
